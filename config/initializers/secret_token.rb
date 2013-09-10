4781797d9b0edd173ca0b967acc27aa58a3f562e
Kevin Quach <kevin.quach@gmail.com>
2013-09-10 17:57:25 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Threepo::Application.config.secret_key_base = '14e09fb3796d0fa89c1b49b2658f6b32987676fb3114071b8bb93c538a1a2259748c79e39a7055cc2b2467243d7396923a9636c89340026905f98a86d001f5f4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Threepo::Application.config.session_store :cookie_store, key: '_threepo_session'
